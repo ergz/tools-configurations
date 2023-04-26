7af543be05ba5b1d659d6e55a96b2d6140a5847f
ergz <e.emanuel.rodriguez@gmail.com>
2023-04-25 21:39:56 -0700

    adds themes

--- copy-files-from.bat
+++ copy-files-from.bat
@@ -7,6 +7,7 @@ echo Copying Sublime Text configuration files to %destination_folder%...
 xcopy "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\Preferences.sublime-settings" "%destination_folder%" /I /Y
 xcopy "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\Default (Windows).sublime-keymap" "%destination_folder%" /I /Y
 xcopy "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\Default.sublime-mousemap" "%destination_folder%" /I /Y
+xcopy "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\atelier-themes\base16-atelierestuary.dark.tmTheme" "%destination_folder%" /I /Y
 
 echo Done.
 
--- copy-files-to.bat
+++ copy-files-to.bat
@@ -7,6 +7,7 @@ echo Copying Sublime Text configuration files to their appropriate location...
 xcopy "%source_folder%\Preferences.sublime-settings" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
 xcopy "%source_folder%\Default (Windows).sublime-keymap" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
 xcopy "%source_folder%\Default.sublime-mousemap" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
+xcopy "%source_folder%\atelier-themes\base16-atelierestuary.dark.tmTheme" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\atelier-themes" /I /Y
 
 echo Copying Clink configuration files to their appropriate location...
 
