7296a447f91b11cb2d8ff1b08ba8e2186fd058ef
ergz <e.emanuel.rodriguez@gmail.com>
2023-04-25 20:24:52 -0700

    new script to copy files to when setting up a new machine

---
+++ copy-files-to.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set "source_folder=D:\tools-configurations"
+
+echo Copying Sublime Text configuration files to their appropriate location...
+
+xcopy "%source_folder%\Preferences.sublime-settings" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
+xcopy "%source_folder%\Default (Windows).sublime-keymap" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
+xcopy "%source_folder%\Default.sublime-mousemap" "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User" /I /Y
+
+echo Copying Clink configuration files to their appropriate location...
+
+xcopy "%source_folder%\flexprompt_config.lua" "C:\Users\%USERNAME%\AppData\Local\clink" /I /Y
+xcopy "%source_folder%\clink_settings" "C:\Users\%USERNAME%\AppData\Local\clink" /I /Y
+
+echo Done.
