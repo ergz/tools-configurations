7e4ab34cffaa21cdf26b49a3c8792188e5954938
ergz <emnlrodriguez@gmail.com>
2023-10-21 19:16:14 -0700

    adds ahk stuff

---
+++ AHK/launchers.ahk
@@ -0,0 +1,35 @@
+﻿; Launch or activate Windows Terminal with F1
+F1::
+{
+    if (WinExist("ahk_class CASCADIA_HOSTING_WINDOW_CLASS"))
+    {
+        if (WinActive("ahk_class CASCADIA_HOSTING_WINDOW_CLASS"))
+        {
+            WinMinimize()
+        }
+        else
+        {
+            WinActivate()
+        }
+    }
+    else
+    {
+        Run("wt")
+    }
+    return
+}
+
+; Launch or activate Google Chrome with F2
+F2::
+{
+    if (WinExist("ahk_class Chrome_WidgetWin_1"))
+    {
+        WinActivate()
+    }
+    else
+    {
+        Run("chrome")
+    }
+    return
+}
+
--- copy-files-from-computer.bat
+++ copy-files-from-computer.bat
@@ -3,6 +3,7 @@
 set "sublime_destination_folder=D:\tools-configurations\sublime-configs"
 set "clink_destination_folder=D:\tools-configurations\clink-configs"
 set "vscode_destination_folder=D:\tools-configurations\vscode-configs"
+set "ahk_destination_folder"="D:\tools-configurations\AHK"
 
 echo Copying Sublime Text user files to %sublime_destination_folder%...
 xcopy "C:\Users\%USERNAME%\AppData\Roaming\Sublime Text\Packages\User\*" "%sublime_destination_folder%" /E /I /Y
@@ -21,3 +22,6 @@ echo Copying vs code user files to %vscode_destination_folder%...
 xcopy "C:\Users\emanuel\AppData\Roaming\Code\User\settings.json" "%vscode_destination_folder%" /I /Y
 
 echo Done.
+
+echo Copying ahk folder 
+xcopy "C:\Users\emanuel\Documents\AutoHotkey\*" "%ahk_destination_folder%" /E /I /Y 
