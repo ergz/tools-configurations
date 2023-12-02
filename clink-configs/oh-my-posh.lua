886c114539d9c1f4146facb3cc4ef110db6a8ac5
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-12-01 16:11:12 -0800

    adds env var

--- clink-configs/oh-my-posh.lua
+++ clink-configs/oh-my-posh.lua
@@ -1,2 +1,5 @@
-load(io.popen('oh-my-posh init cmd --config C:/Users/emanuel/Documents/ohmyposh-themes/zash.omp.json'):read("*a"))()
+local username = os.getenv("USERNAME")
+local configPath = "C:/Users/" .. username .. "/Documents/ohmyposh-themes/zash.omp.json"
+load(io.popen('oh-my-posh init cmd --config "' .. configPath .. '"'):read("*a"))()
+
 -- load(io.popen('oh-my-posh init cmd --config C:/Users/emanuel/Documents/ohmyposh-themes/onehalf.minimal.omp.json'):read("*a"))()
