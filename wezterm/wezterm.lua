33240bbbbfc50c540e513235ff2e4de9d0c00bc4
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-02-27 18:01:52 -0800

    some changes to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,11 @@ if wezterm.config_builder() then
     config = wezterm.config_builder()
 end
 config.font = wezterm.font "BerkeleyMono Nerd Font"
-config.font_size = 14
+config.font_size = 15
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.default_cursor_style = "SteadyBlock"
 config.keys = {
+
     -- This binds Ctrl-Shift-R to reload the configuration
     {
       key = "R",
