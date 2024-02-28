5f85886c612d3de1acde080b6bf0f1ac822a7f8c
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-02-27 16:30:00 -0800

    start using wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require "wezterm"
+
+local config = {}
+
+if wezterm.config_builder() then
+    config = wezterm.config_builder()
+end
+config.font = wezterm.font "BerkeleyMono Nerd Font"
+config.font_size = 14
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.keys = {
+    -- This binds Ctrl-Shift-R to reload the configuration
+    {
+      key = "R",
+      mods = "CTRL|SHIFT",
+      action = "ReloadConfiguration",
+    },
+  }
+
+return config
