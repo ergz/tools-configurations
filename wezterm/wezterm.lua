7b079dfcd6a32d040e388ba5d5f9bdf44996916a
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-03-12 13:24:00 -0700

    adds wsl domain to wez

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,6 +21,20 @@ config.initial_rows = 24 * 2
 config.initial_cols = 80 * 2
 config.window_padding = {left = 1, right = 0, top = 0, bottom = 0}
 config.use_fancy_tab_bar = false
+config.wsl_domains = {
+  {
+    name = 'WSL:Ubuntu-20.04',
+    distribution = 'Ubuntu',
+    default_cwd = "/mnt/c/Users/emanuel"
+
+    -- The default command to run, if the SpawnCommand doesn't otherwise
+    -- override it.  Note that you may prefer to use `chsh` to set the
+    -- default shell for your user inside WSL to avoid needing to
+    -- specify it here
+
+    -- default_prog = {"fish"}
+  },
+}
 config.keys = {
 
     -- This binds Ctrl-Shift-R to reload the configuration
@@ -30,5 +44,4 @@ config.keys = {
       action = "ReloadConfiguration",
     },
   }
-
 return config
