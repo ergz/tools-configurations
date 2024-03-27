26c08df83110ea7f92185b264ef280a08b6f45f0
ergz <emnlrodriguez@gmail.com>
2024-03-26 17:13:41 -0700

    adds come lazygit configs

--- make-links.bat
+++ make-links.bat
@@ -1,23 +1,26 @@
 :: vs code
-mklink "%APPDATA%\Code\User\settings.json" "vscode-configs\settings.json"
+mklink /Y "%APPDATA%\Code\User\settings.json" "vscode-configs\settings.json"
 
 :: clink
-mklink "%LOCALAPPDATA%\clink\.inputrc" "%cd%\clink-configs\.inputrc"
-mklink "%LOCALAPPDATA%\clink\clink_settings" "%cd%\clink-configs\clink_settings"
-mklink "%LOCALAPPDATA%\clink\flexprompt_config.lua" "%cd%\clink-configs\flexprompt_config.lua"
+mklink /Y "%LOCALAPPDATA%\clink\.inputrc" "%cd%\clink-configs\.inputrc"
+mklink /Y "%LOCALAPPDATA%\clink\clink_settings" "%cd%\clink-configs\clink_settings"
+mklink /Y "%LOCALAPPDATA%\clink\flexprompt_config.lua" "%cd%\clink-configs\flexprompt_config.lua"
 :: oh my posh with clink
-mklink "%LOCALAPPDATA%\clink\oh-my-posh.lua" "%cd%\clink-configs\oh-my-posh.lua"
+mklink /Y "%LOCALAPPDATA%\clink\oh-my-posh.lua" "%cd%\clink-configs\oh-my-posh.lua"
 :: oh my posh themes
 
 
 :: sublime text
-mklink "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%cd%\sublime-configs\Preferences.sublime-settings"
+mklink /Y "%APPDATA%\Sublime Text\Packages\User\Preferences.sublime-settings" "%cd%\sublime-configs\Preferences.sublime-settings"
 
 :: alacrity 
-mklink "C:\Users\%USERNAME%\scoop\persist\alacritty\alacritty.toml" "%cd%\alacritty\alacritty.toml"
+mklink /Y "C:\Users\%USERNAME%\scoop\persist\alacritty\alacritty.toml" "%cd%\alacritty\alacritty.toml"
 
 :: wezterm
-mklink "C:\Users\%USERNAME%\.config\wezterm\wezterm.lua" "%cd%\wezterm\wezterm.lua"
+mklink /Y "C:\Users\%USERNAME%\.config\wezterm\wezterm.lua" "%cd%\wezterm\wezterm.lua"
 
 :: windows terminal
-mklink "C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "%cd%\windows-terminal\settings.json"
+mklink /Y "C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "%cd%\windows-terminal\settings.json"
+
+:: lazygit config file
+mklink /Y "C:\Users\%USERNAME%\AppData\Local\lazygit\config.yml" "%cd%\lazygit\config.yml"
