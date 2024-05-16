742254adc78d7912b624a3b1e0f8bfaf49cf0d73
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-05-16 12:38:06 -0700

    updates several things

--- clink-configs/oh-my-posh.lua
+++ clink-configs/oh-my-posh.lua
@@ -1,5 +1,5 @@
 local username = os.getenv("USERNAME")
-local configPath = "C:/Users/" .. username .. "/Documents/ohmyposh-themes/zash.omp.json"
+local configPath = "C:/Users/" .. username .. "/Documents/ohmyposh-themes/pure.json"
 load(io.popen('oh-my-posh init cmd --config "' .. configPath .. '"'):read("*a"))()
 
 os.setalias("ls", "ls --color=auto $*")
