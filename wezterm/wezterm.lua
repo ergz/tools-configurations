bacf83fe082d60decf89bb74ec9b13226f6c8d46
Emanuel Rodriguez <erodriguez@flowwest.com>
2024-05-24 22:02:54 -0700

    more changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -111,12 +111,29 @@ config.keys = {
         action = wezterm.action.SendKey {
             key = "End"
         }
-    }
+    },
+    { key = 'l', mods = 'ALT', action = wezterm.action.ShowTabNavigator },
+     {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+
 
   }
 
 config.audible_bell = "Disabled"
-
+config.tab_max_width = 50
 config.mouse_bindings = {
   -- Ctrl + Click to open links using the system default browser
     {
